92816a715c0da6aee10a27e7281a5e3913395d79
MohitKS5 <mohitkumarsingh907@gmail.com>
2018-09-09 12:54:58 +0530

    add login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  auth;
+  db;
+
+  constructor() {
+    firebase.initializeApp(environment.firebase);
+  }
+
+  ngOnInit() {
+    this.db = firebase.firestore();
+    this.auth = firebase.auth();
+    this.db.settings({
+      timestampsInSnapshots: true
+    });
+    this.auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log('requesting data at fbusers/' + user.uid);
+        this.db.doc(`fbusers/${user.uid}`).get((doc) => {
+          if (doc.exists) {
+            console.log('data: ', doc.data());
+          } else {
+            console.log('doc doesn\'t exist');
+          }
+        });
+      } else {
+        console.log('no user');
+      }
+    });
+  }
+
+  verifydoc() {
+    this.db.collection('fbusers').get().then((docs) => {
+      docs.forEach(function (doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, ' => ', doc.data());
+      });
+    });
+  }
+
+  register() {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then((res: any) =>
+      res.additionalUserInfo.isNewUser ? this.db.collection('fbusers').add({
+        uid: res.user.uid,
+        name: res.additionalUserInfo.profile.name,
+        email: {
+          value: res.additionalUserInfo.profile.email ? res.additionalUserInfo.profile.email : '',
+          verified: res.user.emailVerified ? res.user.emailVerified : '',
+        },
+        facebook: {
+          Token: res.credential.accessToken,
+        },
+        personal: {
+          gender: res.additionalUserInfo.profile.gender ? res.additionalUserInfo.profile.gender : '',
+          phoneNumber: res.user.phoneNumber ? res.user.phoneNumber : '',
+          picture: res.additionalUserInfo.profile.picture ? res.additionalUserInfo.profile.picture : '',
+          birthday: res.additionalUserInfo.profile.birthday ? res.additionalUserInfo.profile.birthday : ''
+        },
+      }).then(() => console.log('register complete')).catch((err) => console.log(err)) : console.log('already loggedin'));
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  firebase: {
+    apiKey: 'AIzaSyBPx6rP71GFqLYIhgoli5K_E8PKTLx5vZg',
+    authDomain: 'antaragni-a3137.firebaseapp.com',
+    databaseURL: 'https://antaragni-a3137.firebaseio.com',
+    projectId: 'antaragni-a3137',
+    storageBucket: 'antaragni-a3137.appspot.com',
+    messagingSenderId: '439012127035'
+  },
+  production: false,
+  fbAppID: '266808170392437',
+  // fbAppID: '1699957190266910',
 };
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
